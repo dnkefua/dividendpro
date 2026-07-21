4b35a3455384aba3510aed51a96933f9f27d07b2
dnkefua <nkefuan@yahoo.com>
2026-07-21 16:04:56 +0400

    chore: configure build variables for static deploy and add firebase to gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.NODE_ENV === 'production' ? '/dividendpro/' : '/',
+    base: process.env.VITE_BASE || '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
